691b360a8e5e8d61112f60865723b12055cc81f9
tam.tahirli@gmail.com <tam.tahirli@gmail.com>
2021-04-16 01:32:21 -0400

    Initial commit, working spring arm, 3d model and movement controller on a ground plane

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+	[SerializeField] private Animator m_animator;
+	[SerializeField] private float m_fMovementSpeed = 1f;
+    [SerializeField] private float m_fTopSpeed = 20f;
+    [SerializeField] private float m_fJumpForce = 5f;
+	[SerializeField] private float m_fSlowDownScale = 0.99f;
+	[SerializeField] private LayerMask m_lGroundedMask;
+
+	private Rigidbody m_rBody;
+	private BoxCollider m_bCollider;
+	private float m_fHalfHeight;
+	private Vector3 m_vStartScale;
+	[SerializeField] private bool m_bGrounded;
+
+    void Awake()
+    {
+		m_rBody = GetComponent<Rigidbody>();
+		m_bCollider = GetComponent<BoxCollider>();
+		m_fHalfHeight = m_bCollider.bounds.extents.y;
+		m_vStartScale = transform.localScale;
+	}
+
+    private void FixedUpdate()
+    {
+		m_bGrounded = CheckGrounded();
+		float fHorizontal = Input.GetAxis("Horizontal");
+
+		m_rBody.AddForce(Vector3.right * fHorizontal, ForceMode.VelocityChange); // Add force on rigid body based on horizontal movement
+
+		m_rBody.velocity *= m_fSlowDownScale; // Slow down player every frame to avoid slideyness
+
+		float fAbsX = Mathf.Abs(m_rBody.velocity.x);
+		m_animator.SetFloat("Movement", fAbsX / m_fTopSpeed); // Set animator float from scale 0-1.
+
+		if(fAbsX > m_fTopSpeed) // Only use top speed for lateral movement
+        {
+			Vector3 velocity = m_rBody.velocity;
+			velocity.x = velocity.x < 0 ? -m_fTopSpeed : m_fTopSpeed;
+			m_rBody.velocity = velocity;
+        }
+
+		transform.localScale = fHorizontal == 0 ? transform.localScale : fHorizontal < 0 ? new Vector3(-m_vStartScale.x, m_vStartScale.y, m_vStartScale.z) : new Vector3(m_vStartScale.x, m_vStartScale.y, m_vStartScale.z);
+		
+		// Jump only if grounded.
+		if (m_bGrounded)
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				m_rBody.AddForce(Vector3.up * m_fJumpForce, ForceMode.VelocityChange);
+				m_animator.SetTrigger("Jump");
+			}
+		}
+	}
+
+	bool CheckGrounded()
+    {
+		return Physics.Raycast(m_bCollider.bounds.center, Vector3.down, m_fHalfHeight + 0.01f, m_lGroundedMask);
+    }
+}
---
+++ Assets/_Scripts/SpringArm.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringArm : MonoBehaviour
+{
+    [SerializeField] private Transform m_target = null;
+
+	[SerializeField] private Vector3 m_vTargetOffset = Vector3.zero;
+    [SerializeField] private float m_fCameraLagSpeed = 10.0f;
+    [SerializeField] private float m_fCameraLagMaxTimeStep = 1.0f / 60.0f;
+    [SerializeField] private float m_fCameraLagMaxDistance = 0.0f;
+
+	/** Temporary variables when using camera lag, to record previous camera position */
+	private Vector3 m_fPreviousDesiredPos;
+
+    private void Start()
+    {
+		UpdateSpringArm(0);
+    }
+
+    private void FixedUpdate()
+    {
+		UpdateSpringArm(Time.deltaTime);
+    }
+
+	void UpdateSpringArm(float DeltaTime)
+	{
+		// Get the spring arm 'origin', the target we want to look at
+		Vector3 ArmOrigin = m_target.position + m_vTargetOffset;
+		Vector3 DesiredLoc = ArmOrigin;
+
+		// Lerp target offset with lag like a spring
+		if (DeltaTime > m_fCameraLagMaxTimeStep && m_fCameraLagSpeed > 0.0f)
+		{
+			Vector3 ArmMovementStep = (DesiredLoc - m_fPreviousDesiredPos) * (1.0f / DeltaTime);
+			Vector3 LerpTarget = m_fPreviousDesiredPos;
+
+			float RemainingTime = DeltaTime;
+			while (RemainingTime > Mathf.Epsilon)
+			{
+				float LerpAmount = Mathf.Min(m_fCameraLagMaxTimeStep, RemainingTime);
+				LerpTarget += ArmMovementStep * LerpAmount;
+				RemainingTime -= LerpAmount;
+
+				DesiredLoc = Vector3.Lerp(m_fPreviousDesiredPos, LerpTarget, LerpAmount * m_fCameraLagSpeed);
+				m_fPreviousDesiredPos = DesiredLoc;
+			}
+		}
+		else
+		{
+			DesiredLoc = Vector3.Lerp(m_fPreviousDesiredPos, DesiredLoc, DeltaTime * m_fCameraLagSpeed);
+		}
+
+		// Clamp distance
+		if (m_fCameraLagMaxDistance > 0.0f)
+		{
+			Vector3 FromOrigin = DesiredLoc - ArmOrigin;
+			if (FromOrigin.magnitude > (m_fCameraLagMaxDistance * m_fCameraLagMaxDistance))
+			{
+				DesiredLoc = ArmOrigin + GetClampedToMaxSize(FromOrigin, m_fCameraLagMaxDistance);
+			}
+
+		}
+
+		m_fPreviousDesiredPos = DesiredLoc;
+
+		transform.position = DesiredLoc;
+	}
+
+	Vector3 GetClampedToMaxSize(Vector3 vector, float MaxSize)
+	{
+		if (MaxSize < Mathf.Epsilon)
+		{
+			return Vector3.zero;
+		}
+
+		float VSq = vector.magnitude;
+		if (VSq > MaxSize * MaxSize)
+		{
+			float Scale = MaxSize * Mathf.Sqrt(VSq);
+			return new Vector3(vector.x * Scale, vector.y * Scale, vector.z * Scale);
+		}
+		else
+		{
+			return vector;
+		}	
+	}
+}
